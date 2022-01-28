20648d069b6659b38cdfe450d1b0acb763a6b57e
Kei Takahashi <takahashi.kgs@om.asahi-kasei.co.jp>
2022-01-28 15:34:40 +0900

    add/feature users

--- test-proj/dist/app.module.js
+++ test-proj/dist/app.module.js
@@ -10,11 +10,12 @@ exports.AppModule = void 0;
 const common_1 = require("@nestjs/common");
 const app_controller_1 = require("./app.controller");
 const app_service_1 = require("./app.service");
+const users_module_1 = require("./users/users.module");
 let AppModule = class AppModule {
 };
 AppModule = __decorate([
     (0, common_1.Module)({
-        imports: [],
+        imports: [users_module_1.UsersModule],
         controllers: [app_controller_1.AppController],
         providers: [app_service_1.AppService],
     })
