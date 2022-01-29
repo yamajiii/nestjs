7f7d701d62fc51d2794a791f3e0a3174ba375840
Kei Takahashi <takahashi.kgs@om.asahi-kasei.co.jp>
2022-01-29 09:30:51 +0900

    users validation

--- test-proj/src/users/dto/create-user.dto.ts
+++ test-proj/src/users/dto/create-user.dto.ts
@@ -1,4 +1,12 @@
+import { IsString, MinLength, MaxLength } from 'class-validator';
 export class CreateUserDto {
+  @IsString()
+  @MinLength(1)
+  @MaxLength(20)
   readonly name: string;
+
+  @IsString()
+  @MinLength(6)
+  @MaxLength(20)
   readonly password: string;
 }
