b944592e217b78b9ce8a474e27016b59f240b68d
Kei Takahashi <takahashi.kgs@om.asahi-kasei.co.jp>
2022-02-05 19:22:59 +0900

    fix cats

--- test-proj/src/app.module.ts
+++ test-proj/src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { CatsController } from './cats/cats.controller';
 
 @Module({
   imports: [UsersModule],
-  controllers: [AppController],
+  controllers: [AppController, CatsController],
   providers: [AppService],
 })
 export class AppModule {}
